682e0cce4a3b49c3b08f8b654270b376e920242e
Santanu Maiti <santanumaiti281@gmail.com>
2026-07-30 12:45:52 +0530

    deleated the accidental duplicate fiels

--- src/MsgBox.jsx
+++
@@ -1,8 +0,0 @@
-export default function MsgBox({userName, textColor}){
-    // let newcolor = color;
-    return (
-        <div>
-            <h1 style={{color:textColor}}>hello {userName}</h1>
-        </div>
-    );
-}
