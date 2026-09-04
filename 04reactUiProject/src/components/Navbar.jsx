ea096d17ac0896f037bdf1130a7103670d9938a0
Santanu Maiti <santanumaiti281@gmail.com>
2026-09-04 13:19:43 +0530

    small changes in button, time for break

--- 04reactUiProject/src/components/Navbar.jsx
+++ 04reactUiProject/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-6 px-16'>
-        <h1 className='bg-black text-white uppercase px-6 py-2 rounded-full'>Target Audience</h1>
-        <h1 className='bg-gray-200 uppercase px-6 py-2 rounded-full tracking-wider'>Digital Banking Platform</h1>
+        <h1 className='bg-black text-white uppercase px-6 py-2 rounded-full cursor-pointer'>Target Audience</h1>
+        <h1 className='bg-gray-200 uppercase px-6 py-2 rounded-full tracking-wider cursor-pointer'>Digital Banking Platform</h1>
     </div>
   )
 }
